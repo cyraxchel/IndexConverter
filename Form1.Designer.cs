763db903d26439bc9f81b10cf2eb544781d2b02f
cyraxchel <cyraxchel@list.ru>
2019-06-23 00:18:39 +0500

    initial load

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿namespace OldIndexConverter {
+    partial class Form1 {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent() {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.данныеToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.загрузитьИзФайлаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.вставитьИзБуфераОбменаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.сохранитьTochtmToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.командаToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.конвертироватьToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.menuStrip1.SuspendLayout();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.данныеToolStripMenuItem,
+            this.командаToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // данныеToolStripMenuItem
+            // 
+            this.данныеToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.загрузитьИзФайлаToolStripMenuItem,
+            this.вставитьИзБуфераОбменаToolStripMenuItem,
+            this.toolStripSeparator1,
+            this.сохранитьTochtmToolStripMenuItem});
+            this.данныеToolStripMenuItem.Name = "данныеToolStripMenuItem";
+            this.данныеToolStripMenuItem.Size = new System.Drawing.Size(62, 20);
+            this.данныеToolStripMenuItem.Text = "Данные";
+            // 
+            // загрузитьИзФайлаToolStripMenuItem
+            // 
+            this.загрузитьИзФайлаToolStripMenuItem.Name = "загрузитьИзФайлаToolStripMenuItem";
+            this.загрузитьИзФайлаToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.загрузитьИзФайлаToolStripMenuItem.Text = "Загрузить из файла";
+            // 
+            // вставитьИзБуфераОбменаToolStripMenuItem
+            // 
+            this.вставитьИзБуфераОбменаToolStripMenuItem.Name = "вставитьИзБуфераОбменаToolStripMenuItem";
+            this.вставитьИзБуфераОбменаToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.вставитьИзБуфераОбменаToolStripMenuItem.Text = "Вставить из буфера обмена";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(223, 6);
+            // 
+            // сохранитьTochtmToolStripMenuItem
+            // 
+            this.сохранитьTochtmToolStripMenuItem.Name = "сохранитьTochtmToolStripMenuItem";
+            this.сохранитьTochtmToolStripMenuItem.Size = new System.Drawing.Size(226, 22);
+            this.сохранитьTochtmToolStripMenuItem.Text = "Сохранить toc.htm";
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.richTextBox1);
+            this.panel1.Location = new System.Drawing.Point(13, 28);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(775, 410);
+            this.panel1.TabIndex = 1;
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(4, 3);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(768, 404);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // командаToolStripMenuItem
+            // 
+            this.командаToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.конвертироватьToolStripMenuItem});
+            this.командаToolStripMenuItem.Name = "командаToolStripMenuItem";
+            this.командаToolStripMenuItem.Size = new System.Drawing.Size(67, 20);
+            this.командаToolStripMenuItem.Text = "Команда";
+            // 
+            // конвертироватьToolStripMenuItem
+            // 
+            this.конвертироватьToolStripMenuItem.Name = "конвертироватьToolStripMenuItem";
+            this.конвертироватьToolStripMenuItem.Size = new System.Drawing.Size(180, 22);
+            this.конвертироватьToolStripMenuItem.Text = "Конвертировать";
+            this.конвертироватьToolStripMenuItem.Click += new System.EventHandler(this.конвертироватьToolStripMenuItem_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "Form1";
+            this.Text = "Конвертор индексных файлов";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.panel1.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem данныеToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem загрузитьИзФайлаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem вставитьИзБуфераОбменаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripMenuItem сохранитьTochtmToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem командаToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem конвертироватьToolStripMenuItem;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Diagnostics;
+
+namespace OldIndexConverter {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Преобразовать текущий файл в HTML
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void конвертироватьToolStripMenuItem_Click(object sender, EventArgs e) {
+            string inputText = richTextBox1.Text;
+            Litem[] tocArray;
+            
+            if (inputText == string.Empty) {
+                MessageBox.Show("Текста нет.");
+            } else {
+                //parce text
+                string[] delim = new string[] { "\r", "\n" };
+                string[] lines = inputText.Split(delim, StringSplitOptions.RemoveEmptyEntries);
+                tocArray = new Litem[lines.Length];
+                string newTOC = "";
+
+                for (int i = 0; i < lines.Length; i++) {
+                    //Debug.WriteLine();
+                    Litem litem = new Litem(lines[i]);
+                    tocArray[i] = litem;
+                    newTOC += litem.GetHTMLView();
+                }
+                
+                richTextBox1.Text = newTOC;
+            }
+        }
+        protected class Litem {
+            //Проверяет, надо ли удалить нумерацию.
+            public static bool replaceNumeric;
+            public static string headerTemplate = @"<br /><strong>{1}</strong><br />";
+            public static string itemtemplate = @"<LI><A href=""{0}"">{1}</A></LI>";
+
+            public string Link;
+            public string Text = "";
+            public bool isHeader = false;
+
+            public Litem(string line) {
+                string[] delim = new string[] { "::" };
+                string[] args = line.Split(delim, StringSplitOptions.None);
+                //TODO detect is header
+                if(args.Length < 2 || args[1].Contains("index")) {
+                    isHeader = true;
+                }
+
+                Text = args[0];
+                if(args.Length > 1) {
+                    Link = args[1];
+                }
+                CheckLink(out Link);
+            }
+
+            private void CheckLink(out string link) {
+                if(link.Contains("htm")) {
+
+                }
+            }
+
+            public string GetHTMLView() {
+                string result = isHeader ? headerTemplate : itemtemplate;
+                result = String.Format(result, Link, Text);
+                /*if (isHeader) {
+                    result = String.Format(result, Text);
+                } else {
+                    result = String.Format(result, Link, Text);
+                }/**/
+                
+                return result + Environment.NewLine;
+            }
+        }
+    }
+}
